3a73fc64796fda0fc288fc962f09234b73f081c6
amagnan <arnauze@gmail.com>
2019-09-24 17:25:32 -0700

    Making the rep public

--- app/src/Components/MainPage.js
+++ app/src/Components/MainPage.js
@@ -3,27 +3,9 @@ import TopBar from './TopBar'
 import { connect } from 'react-redux'
 import MainPart from './MainPart'
 import Amplify, { Auth, API } from 'aws-amplify'
+import { configs } from '../Constants/Constants'
 
-Amplify.configure({
-    Auth: {
-        region: 'us-east-2',
-        userPoolId: 'us-east-2_VgkxVtNIq',
-        userPoolWebClientId: '6oe8l0f5sarangjassslpj61bl'
-    },
-    API: {
-        endpoints: [
-            {
-                name: "Camagru",
-                endpoint: "https://qffrfbpwie.execute-api.us-east-2.amazonaws.com/dev"
-            }
-        ]
-    },
-    Storage: {
-        AWSS3: {
-            bucket: 'camagru-dev', //REQUIRED -  Amazon S3 bucket
-        }
-    }
-})
+Amplify.configure(configs)
 
 class MainPage extends React.Component {
 
